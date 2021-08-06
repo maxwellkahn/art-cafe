379f8721e5084519289fb3dd53bfcde0a3bcd07c
maxwelljkahn <maxwelljkahn@gmail.com>
2021-08-05 22:00:23 -0700

    adds placeholder for ordrHistoryPage

--- src/pages/OrderHistory/OrderHistoryPage.jsx
+++ src/pages/OrderHistory/OrderHistoryPage.jsx
@@ -19,6 +19,7 @@ export default function OrderHistoryPage({paidOrders}) {
   return (
     <>
       <h1>OrderHistoryPage</h1>
+      <ul>Completed Orders:</ul>
       <CompletedOrders paidOrders={paidOrders} />
     </>
   );
